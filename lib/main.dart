7bc8d5838598f1e0bd0deb0e51b7b5faec92ca57
hasanov3598 <bahramov3598@gmail.com>
2023-12-14 17:35:53 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 141, 208, 212),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'First Screen of My apl',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                color: Colors.red,
+                height: 60,
+                width: 60,
+                child: const Center(
+                    child: Text(
+                  '1',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+              Container(
+                color: Colors.yellow,
+                height: 80,
+                width: 80,
+                child: const Center(
+                    child: Text(
+                  '2',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+              Container(
+                color: Colors.green,
+                height: 120,
+                width: 120,
+                child: const Center(
+                    child: Text(
+                  '3',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 60,
+                width: 60,
+                child: const Center(
+                    child: Text(
+                  '1',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+              Container(
+                color: Colors.yellow,
+                height: 80,
+                width: 80,
+                child: const Center(
+                    child: Text(
+                  '2',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+              Container(
+                color: Colors.green,
+                height: 120,
+                width: 120,
+                child: const Center(
+                    child: Text(
+                  '3',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 60,
+                width: 60,
+                child: const Center(
+                    child: Text(
+                  '1',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+              Container(
+                color: Colors.yellow,
+                height: 80,
+                width: 80,
+                child: const Center(
+                    child: Text(
+                  '2',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+              Container(
+                color: Colors.green,
+                height: 120,
+                width: 120,
+                child: const Center(
+                    child: Text(
+                  '3',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+          Container(
+            color: Colors.yellow,
+            height: 90,
+            width: 100,
+            child: const Center(
+                child: Text(
+              '2',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            )),
+          ),
+          Container(
+            color: Colors.green,
+            height: 140,
+            width: 50,
+            child: const Center(
+                child: Text(
+              '3',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            )),
+          ),
+        ]),
+      ),
+    );
+  }
+}
